d96708733e4d20b9dd7954764fe45c52a48e2bc1
Feedbaek <nicek789@naver.com>
2025-01-21 12:38:07 +0900

    Init

---
+++ src/test/java/com/example/k8s_controller/K8sControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8s_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
